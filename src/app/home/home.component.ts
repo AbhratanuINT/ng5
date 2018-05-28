5e90fe5459a9f1bb7de2b955fa801290a45f1498
Abhratanu Mukherjee <abhratanu.mukherjee@indusnet.co.in>
2018-05-28 13:14:26 +0530

    Add files via upload

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number;
+  btnText: string = "Add Item";
+  goalText: string = 'My first Life goal';
+  goals = [];
+  constructor(private _data: DataService) { }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+    this._data.changeGoal(this.goals);
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this._data.changeGoal(this.goals);
+    this.itemCount = this.goals.length;
+  }
+
+}
